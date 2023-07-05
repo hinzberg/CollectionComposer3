825afa14ee289b8db1b7d964b8bd353b6922a1b6
Holger Hinzberg <holger@hinzberg.de>
2023-07-05 20:37:06 +0200

    Initial Commit

---
+++ CollectionComposer3/CollectionComposer3App.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionComposer3App.swift
+//  CollectionComposer3
+//
+//  Created by Holger Hinzberg on 05.07.23.
+//
+
+import SwiftUI
+
+@main
+struct CollectionComposer3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
